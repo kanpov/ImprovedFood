0336d30065ab806be333e24a9f31230808ef9810
RedGrapefruit <karpovanton729@gmail.com>
2021-04-28 17:39:25 +0300

    Added a listener to the salter

--- src/main/java/com/redgrapefruit/improvedfood/screen/handler/SalterScreenHandler.java
+++ src/main/java/com/redgrapefruit/improvedfood/screen/handler/SalterScreenHandler.java
@@ -5,8 +5,12 @@ import com.redgrapefruit.redcore.container.ExtendedContainerScreenHandler;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ArrayPropertyDelegate;
 import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerListener;
+import net.minecraft.util.collection.DefaultedList;
 
 /**
  * A salter screen handler implemented using RedCore.Container library
@@ -34,5 +38,28 @@ public class SalterScreenHandler extends ExtendedContainerScreenHandler {
     @Override
     protected void onListenerInit() {
 
+    }
+
+    private static class Listener implements ScreenHandlerListener {
+        private final PropertyDelegate propertyDelegate;
+
+        public Listener(PropertyDelegate propertyDelegate) {
+            this.propertyDelegate = propertyDelegate;
+        }
+
+        @Override
+        public void onHandlerRegistered(ScreenHandler handler, DefaultedList<ItemStack> stacks) {
+
+        }
+
+        @Override
+        public void onSlotUpdate(ScreenHandler handler, int slotId, ItemStack stack) {
+
+        }
+
+        @Override
+        public void onPropertyUpdate(ScreenHandler handler, int property, int value) {
+
+        }
     }
 }
