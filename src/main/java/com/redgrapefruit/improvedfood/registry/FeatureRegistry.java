13e884f9745f4e491780c3183478e505029cf925
RedGrapefruit <karpovanton729@gmail.com>
2021-05-06 14:52:20 +0300

    Added a FeatureRegistry

---
+++ src/main/java/com/redgrapefruit/improvedfood/registry/FeatureRegistry.java
@@ -0,0 +1,11 @@
+package com.redgrapefruit.improvedfood.registry;
+
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+
+/**
+ * Registry for {@link Feature}s and {@link ConfiguredFeature}s
+ */
+public class FeatureRegistry {
+
+}
